54f8ed7c8698d42f12233f40199d8a2009512b0a
Arfanur Rahman <arfanur36@gmail.com>
2023-03-07 11:47:02 +0600

    2-4 What is global object and modules

---
+++ 4.other.js
@@ -0,0 +1,7 @@
+const name = "programming hero";
+
+const add = (num1, num2) => {
+	return num1 + num2;
+};
+
+module.exports = { name, add };
---
+++ index.js
@@ -0,0 +1,4 @@
+const { add } = require("./4.other");
+
+const result = add(2, 3);
+console.log(result);
