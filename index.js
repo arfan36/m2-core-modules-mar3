1e715c5135a5c5223a5264cff9a04940416b8b5c
Arfanur Rahman <arfanur36@gmail.com>
2023-03-08 09:32:11 +0600

    2-5 Different types of modules

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const { add } = require("./4.other");
+// const { add } = require("./4.other");
 
-const result = add(2, 3);
-console.log(result);
+// core modules
+const http = require("http");
+
+// third party modules -> underscore
+const _ = require("underscore");
+
+const server = http.createServer(function (req, res) {
+	//write code here
+	// console.log(`server is running`);
+});
+
+server.listen(5000);
+
+// const result = add(2, 3);
+// console.log(result);
+
+var stooges = [
+	{ name: "moe", age: 40 },
+	{ name: "larry", age: 50 },
+	{ name: "curly", age: 60 },
+];
+
+const res = _.pluck(stooges, "age");
+console.log(res);
