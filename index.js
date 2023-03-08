ab413131463952af2e2419da1f96c745ef5e80fd
Arfanur Rahman <arfanur36@gmail.com>
2023-03-08 17:57:35 +0600

    2-7 Request and Response Model

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const other = require("./4.other");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	res.end("hello node.js");
+	// console.log(req.url);
+	if (req.url == "/") {
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.write(JSON.stringify({ course: "ACC" }));
+		res.end();
+	} else if (req.url == "/contact") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write("<p>This is contact Page</p>");
+		res.end();
+	}
+	if (req.url == "/about-us") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write("<p>This is About Us Page</p>");
+		res.end();
+	}
 });
 
 const PORT = 5000;
 server.listen(PORT);
 
-console.log(`server is running on port ${PORT}`);
+console.log(`server is running on port at ${PORT}`);
