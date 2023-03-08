4d712580e7ef932cda068dae825d9b4dc7fde08b
Arfanur Rahman <arfanur36@gmail.com>
2023-03-08 20:07:22 +0600

    2-8 Exploring URL Module

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const other = require("./4.other");
-
-// const res = other.subtract(5, 4);
-// console.log("🚀 ~ res:", res);
-
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-	// console.log(req.url);
-	if (req.url == "/") {
-		res.writeHead(200, { "Content-type": "text/html" });
-		res.write(JSON.stringify({ course: "ACC" }));
-		res.end();
-	} else if (req.url == "/contact") {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write("<p>This is contact Page</p>");
-		res.end();
-	}
-	if (req.url == "/about-us") {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write("<p>This is About Us Page</p>");
-		res.end();
-	}
+	const address_url =
+		"http://localhost:5000/contact?name=mezba&country=bangladesh";
+
+	const parsed_url = url.parse(address_url, true);
+	// console.log(parsed_url);
+
+	const queryObject = parsed_url.host;
+	console.log(queryObject);
 });
 
+console.log(url);
 const PORT = 5000;
 server.listen(PORT);
-
 console.log(`server is running on port at ${PORT}`);
+
+// /contact?name=mezba&country=bangladesh
+// /admin
+// /home
